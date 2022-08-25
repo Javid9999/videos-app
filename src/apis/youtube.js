8640d3b2fb15e0b1a67f3db18f4dc562d59693d9
Javid9999 <javidalizadee@gmail.com>
2022-08-25 14:25:50 +0400

    finish

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,13 +1,2 @@
-import axios from 'axios';
 
-const KEY = 'Your API Key'
-
-export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3',
-    params: {
-        part: 'snippet',
-        maxResults: 5, 
-        key: KEY
-    }
-});
 
